90de01631fbb07b38fbf3f4a8b05c12e8e4c417b
David <desmaisons_david@hotmail.com>
2020-08-02 15:08:35 -0300

    Range model binder

--- BeerAPI/BeerAPI/DTO/BeerQuery.cs
+++ BeerAPI/BeerAPI/DTO/BeerQuery.cs
@@ -1,11 +1,21 @@
-﻿namespace BeerAPI.DTO
+﻿using BeerAPI.Infra;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeerAPI.DTO
 {
     public class BeerQuery
     {
         public string Name { get; set; }
+
+        [ModelBinder(BinderType = typeof(RangeModelBinder))]
         public Range Color { get; set; }
+
+        [ModelBinder(BinderType = typeof(RangeModelBinder))]
         public Range AlcoholPercentage { get; set; }
+
+        [ModelBinder(BinderType = typeof(RangeModelBinder))]
         public Range Temperature { get; set; }
+
         public string Ingredient { get; set; }
         public int PageNumber { get; set; }
         public int MaxItems { get; set; }
---
+++ BeerAPI/BeerAPI/Infra/RangeModelBinder.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using BeerAPI.DTO;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Newtonsoft.Json;
+
+namespace BeerAPI.Infra
+{
+    public class RangeModelBinder: IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var modelName = bindingContext.ModelName;
+            var valueProviderResult = bindingContext.ValueProvider.GetValue(modelName);
+
+            if (valueProviderResult == ValueProviderResult.None)
+            {
+                return Task.CompletedTask;
+            }
+
+            var range = JsonConvert.DeserializeObject<Range>(valueProviderResult.FirstValue);
+            bindingContext.Result = ModelBindingResult.Success(range);
+            return Task.CompletedTask;
+        }
+    }
+}
