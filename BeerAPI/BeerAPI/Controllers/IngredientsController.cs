ff6a9742715f5797b91246da83b01fa0b1f4ec71
David <desmaisons_david@hotmail.com>
2020-08-02 17:41:58 -0300

    Add ingredients endpoints

---
+++ BeerAPI/BeerAPI/Controllers/IngredientsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BeerAPI.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IngredientsController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<IngredientResult>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IEnumerable<IngredientResult>> Get()
+        {
+            return new[]
+            {
+                new IngredientResult(1, "água"),
+                new IngredientResult(1, "ovo"),
+                new IngredientResult(1, "malte"),
+                new IngredientResult(1, "lúpulo"),
+                new IngredientResult(1, "arroz")
+            };
+        }
+    }
+}
---
+++ BeerAPI/BeerAPI/DTO/IngredientResult.cs
@@ -0,0 +1,14 @@
+﻿namespace BeerAPI.DTO
+{
+    public class IngredientResult
+    {
+        public string Name { get; }
+        public int Id { get; }
+
+        public IngredientResult(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
