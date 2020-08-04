23e30d13cab005a7a5cdd7f0c9f539e8e542d323
David <desmaisons_david@hotmail.com>
2020-08-04 16:56:09 -0300

    first implementation for beerfinder

--- BeerAPI/BeerAPI.Services/BeerFinder.cs
+++ BeerAPI/BeerAPI.Services/BeerFinder.cs
@@ -1,89 +1,59 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using BeerAPI.Data.Entity;
 using BeerAPI.Services.DTO;
+using NHibernate;
 
 namespace BeerAPI.Services
 {
     public class BeerFinder : IBeerFinder
     {
+        private readonly ISession _Session;
+
+        public BeerFinder(ISession session)
+        {
+            _Session = session;
+        }
+
         public async Task<BeerQueryResult> FindByCriteria(BeerQuery query)
         {
-            return new BeerQueryResult(new[]
-           {
-                new BeerDescriptionResult(1, "Budweiser",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/budweiser_600ml.png"),
-                new BeerDescriptionResult(2, "beck's",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/becks_330ml.png"),
-                new BeerDescriptionResult(3, "Bohemia Imperial",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/bohemia-imperial_550ml.png"),
-                new BeerDescriptionResult(1, "Budweiser",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/budweiser_600ml.png"),
-                new BeerDescriptionResult(2, "beck's",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/becks_330ml.png"),
-                new BeerDescriptionResult(3, "Bohemia Imperial",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/bohemia-imperial_550ml.png"),
-                new BeerDescriptionResult(1, "Budweiser",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/budweiser_600ml.png"),
-                new BeerDescriptionResult(2, "beck's",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/becks_330ml.png"),
-                new BeerDescriptionResult(3, "Bohemia Imperial",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/bohemia-imperial_550ml.png"),
-                new BeerDescriptionResult(1, "Budweiser",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/budweiser_600ml.png"),
-                new BeerDescriptionResult(2, "beck's",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/becks_330ml.png"),
-                new BeerDescriptionResult(3, "Bohemia Imperial",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/bohemia-imperial_550ml.png"),
-                new BeerDescriptionResult(1, "Budweiser",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/budweiser_600ml.png"),
-                new BeerDescriptionResult(2, "beck's",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/becks_330ml.png"),
-                new BeerDescriptionResult(3, "Bohemia Imperial",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/bohemia-imperial_550ml.png"),
-                new BeerDescriptionResult(1, "Budweiser",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/budweiser_600ml.png"),
-                new BeerDescriptionResult(2, "beck's",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/becks_330ml.png"),
-                new BeerDescriptionResult(3, "Bohemia Imperial",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/bohemia-imperial_550ml.png"),
-                new BeerDescriptionResult(1, "Budweiser",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/budweiser_600ml.png"),
-                new BeerDescriptionResult(2, "beck's",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/becks_330ml.png"),
-                new BeerDescriptionResult(3, "Bohemia Imperial",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/bohemia-imperial_550ml.png"),
-                new BeerDescriptionResult(1, "Budweiser",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/budweiser_600ml.png"),
-                new BeerDescriptionResult(2, "beck's",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/becks_330ml.png"),
-                new BeerDescriptionResult(3, "Bohemia Imperial",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/bohemia-imperial_550ml.png"),
-                new BeerDescriptionResult(1, "Budweiser",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/budweiser_600ml.png"),
-                new BeerDescriptionResult(2, "beck's",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/becks_330ml.png"),
-                new BeerDescriptionResult(3, "Bohemia Imperial",
-                    @"https://www.ambev.com.br/conteudo/uploads/2019/03/bohemia-imperial_550ml.png"),
-            }, false);
+            var beers = await _Session.QueryOver<Beer>().ListAsync();
+            var done = beers.Count < query.MaxItems;
+            var beersDto = beers.Select(TransformToDescription);
+            return new BeerQueryResult(beersDto.ToArray(), done);
         }
 
         public async Task<BeerResult> FindById(int id)
         {
-            return Bud(id);
+            var beer = await _Session.LoadAsync<Beer>(id);
+            return Transform(beer);
         }
 
-        private BeerResult Bud(int id = 1)
+        private static BeerDescriptionResult TransformToDescription(Beer beer)
         {
+            var imageUrl = BuildImageFromFile(beer.Image);
+            return new BeerDescriptionResult(beer.Id.Value, beer.Name, imageUrl);
+        }
+
+        private static BeerResult Transform(Beer beer)
+        {
+            var imageUrl = BuildImageFromFile(beer.Image);
             return new BeerResultBuilder()
-                .SetId(id)
-                .SetName("Budweiser")
-                .SetDescription("Só uma cerveja como a Budweiser consegue manter os 143 anos de história e, ao mesmo tempo, ter a autenticidade e liberdade para ser o que quiser. Ela não muda sua fórmula e nunca perde sua essência. Só Bud faz Bud.")
-                .SetAlcoholPercentage(5)
-                .SetHarmonization("O sabor marcante no começo e suave no final de Budweiser pede que ela harmonize com hambúrguer clássico, presunto cozido e mix de castanhas.")
-                .SetTemperature(new Range(0, 4))
-                .SetIngredients(new[] { "água", "malte", "arroz", "lúpulo" })
-                .SetPictureUrl(@"https://www.ambev.com.br/conteudo/uploads/2019/03/budweiser_600ml.png")
-                .SetColor(10)
-                .Build();
+                .SetId(beer.Id.Value)
+                .SetName(beer.Name)
+                .SetDescription(beer.Description)
+                .SetHarmonization(beer.Harmonization)
+                .SetAlcoholPercentage(beer.AlcoholPercentage)
+                .SetColor(beer.Color)
+                .SetTemperature(new Range(beer.MinTemperature, beer.MaxTemperature))
+                .SetIngredients(beer.Ingredients.Select(ing => ing.Name).ToArray())
+                .SetPictureUrl(imageUrl)
+                .Build(); ;
+        }
+
+        private static string BuildImageFromFile(byte[] file)
+        {
+            return "";
         }
     }
 }
